8273b677b7e1ce01ab98fbe2ee8691c27981ce5e
jiangyiqing <jiangyq2004@gmail.com>
2013-11-20 08:28:26 +0800

    update

--- q.js
+++ q.js
@@ -2,5 +2,6 @@ $(document).ready(function(){
 	    $('#sun').click(function(){
    
     	$('#sun').animate({left:"+=50px"});
+    	$('#sun').css("background-image", "-webkit-gradient(linear, 50% 50%, 100% 100%, from(#FF0000), to(#FF7000))")
     });
 });
